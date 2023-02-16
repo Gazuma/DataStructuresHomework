6a3fe8db49859ec7530536aaed8f22a9d5a06ad4
Gazuma <deepraut.work@gmail.com>
2023-02-16 20:07:42 +0530

    Q2 Solved
    Solved the question to find the sum of all data values of linked list

---
+++ sumOfLinkedList.c
@@ -0,0 +1,62 @@
+//Problem Statement : Sum all the data in linked list and return
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+typedef struct node llnode;
+
+llnode* createNode(){
+    llnode* newNode = (llnode*)malloc(sizeof(llnode));
+    newNode->next = NULL; //Good Practice🙂
+    printf("Enter data for new node : ");
+    scanf("%d",&newNode->data);
+    return newNode;
+}
+
+llnode* createLL(int numberOfNodes){
+    llnode* head = createNode();
+    llnode* temp = head;
+    for(int i =1;i<numberOfNodes;i++){
+        temp->next = createNode();
+        temp = temp->next;
+    }
+    return head;
+}
+
+int sumLL(llnode* head){
+    int sum = 0;
+    if(head==NULL){
+        printf("Linked list is empty\n");
+        return sum;
+    }
+    while(head){
+        sum+=head->data;
+        head = head->next;
+    }
+    return sum;
+}
+
+void printLL(llnode* head){
+    if(head == NULL)
+        return;
+    while(head){
+        printf("%d->",head->data);
+        head = head->next;
+    }
+}
+
+int main(){
+    llnode* head; //Variable to hold base address of linked list
+    int numberOfNodes; //Variable to take input parameter for creating linked list
+    printf("Enter the number of nodes to create for linked list : ");
+    scanf("%d",&numberOfNodes);
+    head = createLL(numberOfNodes);
+    printf("Sum of nodes in linked list : %d\n",sumLL(head));
+    printf("Given linked list : ");
+    printLL(head);
+    return 0;
+}
